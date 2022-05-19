99613824861e4da38b31832864ac5fbbbcb47a9c
jmn-exe <amouxmerle@gmail.com>
2022-05-19 15:58:42 +0800

    design

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function calcDistance(p1,p2){
-    return(Math.sqrt(Math.pow(p1[0]-p2[0],2)+Math.pow(p1[1]-p2[1],2)));
-};
-
 function createMap(){
     var iSizeX = 20;
     var iSizeY = 20;
@@ -46,10 +42,10 @@ function createMap(){
     latlngs.forEach((point)=>{
         markArr.push(L.marker(point).addTo(map));
     })
-    console.log(calcDistance([1.45165, 110.44186],[1.45642, 110.43967]));
+    //console.log(calcDistance([1.45165, 110.44186],[1.45642, 110.43967]));
     var userMark = L.marker([0,0]).addTo(map);
     userMark.setIcon(myIcon);
-    markArr[1].bindPopup('<div style="width: 300px"><img style="width: 300px; height: 200px; object-fit: cover;" src="https://images.pexels.com/photos/65438/pexels-photo-65438.jpeg?cs=srgb&dl=pexels-kaique-rocha-65438.jpg&fm=jpg&w=1280&h=1787"/><div style="font-weight: bold;">Building 1</div><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod interdum tellus.</div><div style="display:flex; align-items: center; vertical-align: center; margin-top:10px;"><a style="flex:1; align-items: center; vertical-align: center;" href=""><img style="width: 80%;" src="3d-icon.png"/></a><a style="flex:1; align-items: center; vertical-align: center;" href=""><img style="width: 80%;" src="360-icons.png"/></a></div></div>');
+    markArr[1].bindPopup('<div style="width: 250px; display:flex; flex-direction: column; justify-content: center; align-items: center;"><img style="width: 250px; height: 150px; object-fit: cover;" src="https://images.pexels.com/photos/65438/pexels-photo-65438.jpeg?cs=srgb&dl=pexels-kaique-rocha-65438.jpg&fm=jpg&w=1280&h=1787"/><div style="font-weight: bold;">Building 1</div><div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam euismod interdum tellus.</div><div style="display:flex; justify-content: center; align-items: center; margin-top:10px;"><div style="display:flex;"><a style="flex:1;display:flex; justify-content: center; align-items: center;" href=""><img style="width: 70%;" src="3d-icon.png"/></a><a style="flex:1;display:flex; justify-content: center; align-items: center;" href=""><img style="width: 70%;" src="360-icons.png"/></a></div></div></div>');
     map.on('popupopen', function(e) {
         var px = map.project(e.popup._latlng);
         px.y -= e.popup._container.clientHeight/2;
@@ -65,12 +61,16 @@ function createMap(){
         markArr.forEach((marker)=>{
             var userPoint = [userMark.getLatLng().lat,userMark.getLatLng().lng];
             var markPoint = [marker.getLatLng().lat,marker.getLatLng().lng];
-            console.log((calcDistance(userPoint,markPoint)));
             if((calcDistance(userPoint,markPoint)) <= 0.0007){
                 marker.openPopup();
             }
         })
     });
+    document.getElementById('centerBtn').onclick = () => {
+        map.panTo(userMark.getLatLng());
+    }
+
+
     document.getElementById('up').onclick = () => {userMark.setLatLng({lat : userMark.getLatLng().lat + 0.00005, lng: userMark.getLatLng().lng});};
     document.getElementById('down').onclick = () => {userMark.setLatLng({lat : userMark.getLatLng().lat - 0.00005, lng: userMark.getLatLng().lng});};
     document.getElementById('left').onclick = () => {userMark.setLatLng({lat : userMark.getLatLng().lat, lng: userMark.getLatLng().lng - 0.00005});};
@@ -96,3 +96,7 @@ function createMap(){
       }, false);
     // zoom the map to the polyline
 }
+
+function calcDistance(p1,p2){
+    return(Math.sqrt(Math.pow(p1[0]-p2[0],2)+Math.pow(p1[1]-p2[1],2)));
+};
